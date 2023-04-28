3cc1cafb307366f1456e4d01c64b72f8029c97e2
Hugofullstack <hugo.n.montefusco@gmail.com>
2023-04-28 14:32:49 -0300

    arreglos_defire

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,16 @@
 export const environment = {
-  firebase: {
-    projectId: 'proyecto-integrador-6f9e4',
-    appId: '1:1008506263447:web:b0a05b8f3804f5a74c16d2',
-    storageBucket: 'proyecto-integrador-6f9e4.appspot.com',
-    locationId: 'southamerica-east1',
-    apiKey: 'AIzaSyDHSc36GYejh5PpO_FKR2yfaH6kTQ_vsZs',
-    authDomain: 'proyecto-integrador-6f9e4.firebaseapp.com',
-    messagingSenderId: '1008506263447',
-    
-  },
+  firebaseConfig = {
+  apiKey: "AIzaSyDHSc36GYejh5PpO_FKR2yfaH6kTQ_vsZs",
+  authDomain: "proyecto-integrador-6f9e4.firebaseapp.com",
+  databaseURL: "https://proyecto-integrador-6f9e4-default-rtdb.firebaseio.com",
+  projectId: "proyecto-integrador-6f9e4",
+  storageBucket: "proyecto-integrador-6f9e4.appspot.com",
+  messagingSenderId: "1008506263447",
+  appId: "1:1008506263447:web:b0a05b8f3804f5a74c16d2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
  
   production: true,
   URL: "https://portfolio-hugo-montefusco.onrender.com/",
